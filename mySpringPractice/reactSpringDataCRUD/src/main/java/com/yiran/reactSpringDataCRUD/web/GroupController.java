101c4cbaf8d13334deb3a357bc1cef6b836fe44a
UNO998 <shenyiran2015@outlook.com>
2019-10-29 17:14:52 -0400

    Created Controller to map url and implemented REST API

---
+++ mySpringPractice/reactSpringDataCRUD/src/main/java/com/yiran/reactSpringDataCRUD/web/GroupController.java
@@ -0,0 +1,61 @@
+package com.yiran.reactSpringDataCRUD.web;
+
+import java.net.URISyntaxException;
+import java.util.Collection;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.yiran.reactSpringDataCRUD.model.Group;
+import com.yiran.reactSpringDataCRUD.model.GroupRepository;
+
+@RestController
+@RequestMapping("/api")
+public class GroupController {
+	
+	private final Logger log = LoggerFactory.getLogger(GroupController.class);
+	private GroupRepository groupRepository;
+	
+	public GroupController(GroupRepository groupRepository) {
+		this.groupRepository = groupRepository;
+	}
+	
+	@GetMapping("/groups")
+	public Collection<Group> groups() {
+		return groupRepository.findAll();
+	}
+	
+	@GetMapping("/group/{id}")
+	public ResponseEntity<?> getGroup(@PathVariable Long id) {
+		Optional<Group> group = groupRepository.findById(id);
+		
+		return group.map(response -> ResponseEntity.ok().body(response))
+				.orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+	}
+	
+	@PostMapping("/group")
+	public ResponseEntity<Group> createGroup(@Valid @RequestBody Group group) throws URISyntaxException {
+		log.info("Request to update group: {}", group);
+		Group result = groupRepository.save(group);
+		return ResponseEntity.ok().body(result);
+	}
+	
+	@DeleteMapping("/group/{id}")
+	public ResponseEntity<?> deleteGroup(@PathVariable Long id) {
+		log.info("Request to delete group: {}", id);
+		groupRepository.deleteById(id);
+		return ResponseEntity.ok().build();
+	}
+}
