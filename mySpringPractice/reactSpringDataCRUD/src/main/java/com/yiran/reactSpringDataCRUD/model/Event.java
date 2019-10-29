7e9b453cc5eb74ef3492c486d34da40c55f9fe07
UNO998 <shenyiran2015@outlook.com>
2019-10-29 15:55:19 -0400

    Finish model building

--- mySpringPractice/reactSpringDataCRUD/src/main/java/com/yiran/reactSpringDataCRUD/model/Event.java
+++ mySpringPractice/reactSpringDataCRUD/src/main/java/com/yiran/reactSpringDataCRUD/model/Event.java
@@ -3,19 +3,28 @@ package com.yiran.reactSpringDataCRUD.model;
 import java.time.Instant;
 import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 
 @Data
 @NoArgsConstructor
-@RequiredArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Event {
 
+	@Id
+	@GeneratedValue
 	private Long id;
 	private Instant date;
 	private String title;
 	private String description;
 	
+	@ManyToMany
 	private Set<User> attendees;
 }
--- mySpringPractice/reactSpringDataCRUD/src/main/java/com/yiran/reactSpringDataCRUD/model/User.java
+++ mySpringPractice/reactSpringDataCRUD/src/main/java/com/yiran/reactSpringDataCRUD/model/User.java
@@ -1,7 +1,19 @@
 package com.yiran.reactSpringDataCRUD.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class User {
 
+	@Id
 	private String id;
 	private String name;
 	private String email;
